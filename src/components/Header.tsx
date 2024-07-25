9071ab448637548b2f862463240677d6ac11cd04
unknown <nikcevici13@gmail.com>
2024-07-25 20:32:47 +0200

    aaa

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+//@ts-nocheck
+
 "use client";
 
 import React, { useState } from "react";
@@ -19,11 +21,11 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "swiper/css/autoplay";
 import { motion } from "framer-motion";
+
 const Header = () => {
   const [slideDirection, setSlideDirection] = useState("left");
   const [key, setKey] = useState(0); // Key to force re-render
 
-  //@ts-ignore
   const handleSlideChange = (swiper) => {
     if (swiper.activeIndex > swiper.previousIndex) {
       setSlideDirection("left");
@@ -39,14 +41,13 @@ const Header = () => {
     visible: {
       opacity: 1,
       transition: {
-        staggerChildren: 0.3, // Adjust the stagger timing here
+        staggerChildren: 0.3,
         duration: 0.5,
       },
     },
   };
 
   const textVariants = {
-    // @ts-ignore
     hidden: (direction) => ({
       x: direction === "left" ? "-100%" : "100%",
       opacity: 0,
@@ -64,7 +65,7 @@ const Header = () => {
   };
 
   return (
-    <div className="h-[85vh]">
+    <div className="h-[85vh] relative overflow-hidden">
       <Swiper
         modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
         autoplay={{
@@ -84,13 +85,32 @@ const Header = () => {
           "--swiper-navigation-color": "#f97316",
         }}
       >
-        {[Image1, Image2, Image3].map((src, index) => (
-          <SwiperSlide key={index} className="">
-            <div className="relative w-full h-full bg-orange-400">
+        {[
+          {
+            image: Image1,
+            text1: "Pura Vida",
+            text2: "beach longue",
+            text3: "Drobni pijesak",
+          },
+          {
+            image: Image2,
+            text1: "Beachfront",
+            text2: "The Spot",
+            text3: "For Success",
+          },
+          {
+            image: Image3,
+            text1: "Paradise Hub",
+            text2: "The Place",
+            text3: "Of Peace",
+          },
+        ].map((item, index) => (
+          <SwiperSlide key={index} className="relative overflow-hidden">
+            <div className="relative w-full h-full bg-orange-400 overflow-hidden">
               <Image
                 className="w-full h-full object-cover opacity-70"
                 layout="fill"
-                src={src}
+                src={item.image.src}
                 alt={`Heading image ${index + 1}`}
               />
               <div className="absolute inset-0 bg-black opacity-50"></div>
@@ -107,25 +127,25 @@ const Header = () => {
                   variants={textVariants}
                   custom={slideDirection}
                 >
-                  Pura Vida
+                  {item.text1}
                 </motion.h2>
                 <motion.p
                   className="text-2xl mb-4 font-custom"
                   variants={textVariants}
                   custom={slideDirection}
                 >
-                  beach longue
+                  {item.text2}
                 </motion.p>
                 <motion.h3
-                  className=" text-[3rem] sm:text-[4rem] mb-4 leading-[3.5rem]"
+                  className="text-[3rem] sm:text-[4rem] mb-4 leading-[3.5rem]"
                   variants={textVariants}
                   custom={slideDirection}
                 >
-                  Drobni pijesak
+                  {item.text3}
                 </motion.h3>
-                <button className="relative border-2 mt-4 border-orange-500 bg-transparent text-white py-3 px-6 overflow-hidden group">
+                <button className="relative border-2 mt-4 border-white transition-all duration-300 ease-out hover:border-orange-500 bg-transparent text-white py-3 px-6 overflow-hidden group">
                   <span className="relative z-10">Learn More</span>
-                  <span className="absolute inset-0 bg-orange-500 transform opacity-0 group-hover:opacity-100 scale-x-[.7] scale-y-[.9] group-hover:scale-y-100 transition-all duration-300 ease-in-out group-hover:scale-x-100"></span>
+                  <span className="absolute inset-0 bg-orange-500 transform opacity-0 group-hover:opacity-100 scale-x-[.8] scale-y-[.9] group-hover:scale-y-100 transition-all duration-[275ms] ease-out group-hover:scale-x-100"></span>
                 </button>
               </motion.div>
             </div>
