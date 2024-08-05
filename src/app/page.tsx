16a46d5cbe2cabcb456e1488b16e3fe6224cdc13
ivannikcevicc <nikcevici13@gmail.com>
2024-08-05 21:30:54 +0200

    minor tweak

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <About 
               image={BgImage}
               subheading="Welcome"
-              heading="Headinggggg"
+              heading="To Pura Vida"
               text='On August 30, 2022, Drobni Pijesak Beach in Budvanska Rivijera, Montenegro, was honored as one of Europe&apos;s top 40 beaches, a testament to its exceptional charm and allure. This recognition highlights the beach&apos;s appeal to travelers seeking extraordinary coastal destinations. Nestled in a picturesque setting, Drobni Pijesak Beach embodies the essence of &quot;Pura Vida&quot; with its pristine sands and crystal-clear waters. The hidden gem offers a serene escape framed by dramatic cliffs and lush greenery. Its tranquil ambiance, coupled with natural beauty and an idyllic setting, makes it a perfect spot for relaxation and enjoying the unspoiled Adriatic coast.'   buttonText="Learn More"
               buttonLink="https://www.google.com"
       />
